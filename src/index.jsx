4ecface0124915be337fbb30c25c486ceb1b6964
Bobby <s215818@dtu.dk>
2023-06-23 23:56:14 +0200

    Added react

---
+++ src/index.jsx
@@ -0,0 +1,28 @@
+import React from "react"
+import "../styles/style.css"
+
+export const Desktop = () => {
+    return (
+        <div className="desktop">
+            <div className="div">
+                <div className="group">
+                    <h1 className="deniz-isikli">
+                        <span className="text-wrapper">
+                            <br />
+                        </span>
+                        <span className="span">
+                            Software Engineering student
+                            <br />
+                        </span>
+                        <span className="text-wrapper-2">S</span>
+                        <span className="text-wrapper-3">
+                            Student at Denmark's Technical University
+                            <br />
+                            Open Software Enthusiast
+                        </span>
+                    </h1>
+                </div>
+            </div>
+        </div>
+    );
+};
