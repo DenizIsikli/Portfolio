135fb9d148bfa58d0f6704d0a1c2a474b3194117
Bobby <s215818@dtu.dk>
2023-11-11 23:46:35 +0100

    Fix

--- portfolio/src/components/frontpage.tsx
+++ portfolio/src/components/frontpage.tsx
@@ -1,41 +1,42 @@
 import React from 'react';
 import './frontpage.css';
 import { FaGithub, FaLinkedin, FaEnvelope, FaCode } from 'react-icons/fa';
+import Project from './project';
 
 
 const Frontpage: React.FC = () => {
     return (
         <div className="frontpage">
-            <div className="intro-section">
+            <div className="frontpage-section">
                 <h1>Deniz Isikli</h1>
                 <p>Software Engineering student @ Denmark's Technical University</p>
-
+                
                 <section className="socials">
                     <div>
                         <a href="./project.tsx" rel="noopener noreferrer" target="_blank">
-                        Projects<FaCode />
+                            Projects<FaCode />
                         </a>
                     </div>
                     <div>
                         <a href="https://github.com/DenizIsikli" rel="noopener noreferrer" target="_blank">
-                        GitHub<FaGithub />
+                            GitHub<FaGithub />
                         </a>
                     </div>
                     <div>
                         <a href="https://www.linkedin.com/in/deniz-isikli/" rel="noopener noreferrer" target="_blank">
-                        LinkedIn<FaLinkedin />
+                            LinkedIn<FaLinkedin />
                         </a>
                     </div>
                     <div>
                         <a href="mailto:denizisikli7@gmail.com" rel="noopener noreferrer" target="_blank">
-                        Gmail<FaEnvelope />
+                            Gmail<FaEnvelope />
                         </a>
                     </div>
                 </section>
             </div>
         </div>
     );
-    };
+};
 
 export default Frontpage;
   
